0d5c0e32381a4f6a2ec818305e4599a11cc535a2
William Robertson <williamp.robertson@gmail.com>
2019-01-07 00:02:45 -0700

    added deployed link to readme

---
+++ build/precache-manifest.91c71a0d5f3e2a1b3b4e2f29714de35c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ece44e0284ea57204473",
+    "url": "/static/js/main.ece44e02.chunk.js"
+  },
+  {
+    "revision": "d0bba6d1eb76f0560222",
+    "url": "/static/js/1.d0bba6d1.chunk.js"
+  },
+  {
+    "revision": "ece44e0284ea57204473",
+    "url": "/static/css/main.2b210d8e.chunk.css"
+  },
+  {
+    "revision": "c971778e1df855c58114ea32faaccbd3",
+    "url": "/index.html"
+  }
+];
