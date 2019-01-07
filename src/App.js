0f8ce90b7d845f5dd3b0082a190e1691c4b35c06
William Robertson <williamp.robertson@gmail.com>
2019-01-06 23:45:39 -0700

    reddit axios initial build working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
+      const posts = res.data.data.children.map(obj => obj.data);
+      this.setState({ posts });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>/r/reactjs</h1>
+        <ul>
+          {this.state.posts.map(post => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ul>
       </div>
     );
   }
